cdabb48f32a78e3198ca4ff0bc822d1ea7a43217
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-07-05 11:11:45 +0100

    Detailed initialise pi

---
+++ inputs.c
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+// Raspberry Pi GPIO Interface
+// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// File: inputs.c
+// PA Consulting - Lawrence Jones
+///////////////////////////////////////////////////////////////////////////////
+
+// Configure the memory access required to alter
+// the GPIO pins. Will request memory access from the system,
+// note that this requires sudo privileges!
+// Based on example at http://elinux.org/RPi_Low-level_peripherals
+int initialiseGpios()
+{
+  // Open system /dev/mem location for direct mem access
+  int devmem = open("/dev/mem", O_RDWR|O_SYNC);
+  // Verify mem access successful
+  if (devmem < 0)
+  {
+    // Access not successful, print error and exit
+    fprintf(stderr, "Failed to access dev/mem - verify sudo?\n");
+    exit(EXIT_FAILURE);
+  }
+  // Use mmap to map to the gpio direct memory locations
+  gpioMap = mmap(
+    NULL,        // specific address not required
+    BLOCK_SIZE,  // the length of the mapping, to include
+                 // all the gpio reserved space
+    PROT_READ|PROT_WRITE,  // enable RW to memory
+    MAP_SHARED,  // share this location with other processes
+    devmem,      // specific file to open
+    GPIO_BASE    // move to the GPIO reserved space by offset
+  );
+  // Close the filestream
+  close(devmem);
+  // Verify the integrity of the memory mapping
+  if (gpioMap == MAP_FAILED)
+  {
+    // Mapping not successful, print error and exit
+    fprintf(stderr, "Error from builtin mmap - code %d\n", (int) gpioMap);
+    exit(EXIT_FAILURE);
+  }
+  // Use volatile pointer to allow shared access of the gpio locations
+  // Slows performance but refreshing is required
+  gpio = (volatile unsigned *)gpioMap;
+}
--- inputs.h
+++ inputs.h
@@ -1,16 +1,46 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Air Quality Sensor
-// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// Raspberry Pi GPIO Interface
+// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 // File: inputs.h
-// PA Consulting - Joao Fermoselle
+// PA Consulting - Lawrence Jones
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef INPUTS
 #define INPUTS
 
+#include <stdint.h>
+#include <sys/mman.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// DEFINE MACROS
+///////////////////////////////////////////////////////////////////////////////
+
+// Define the 'base' mem addr for the start of gpio access
+#define BCM2708_PERI_BASE               0x20000000u
+// Calculate the base dependent on platform
+#define GPIO_BASE ( BCM2708_PERI_BASE + 0x00200000u )
+// Define macro values for the paging and block size
+#define PAGE_SIZE  (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+// Macros to prep pin for accessing
+// INP_GPIO must be used prior to OUT and SET
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= \
+                    (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+// Macros to set and clear individual pins
+#define GPIO_SET *(gpio +  7) // offset to set word
+#define GPIO_CLR *(gpio + 10) // offset to clear word
+
 // Define unsigned int for reading results
 // 16 bits presumed to be sufficient
 typedef uint16_t u16;
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION STUBS
+///////////////////////////////////////////////////////////////////////////////
 
 // Standard sensor retrieval functions
 u16 getTemperature();
@@ -20,5 +50,8 @@ u16 getPressure();
 u16 getUV();
 u16 getNO2();
 u16 getCO();
+
+// GPIO helpers
+int initialiseGpios();
 
 #endif
