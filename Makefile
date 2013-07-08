8397dfdcefa800cbd3dfde9a39ab73ccb70de0fa
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-07-08 09:33:29 +0000

    Added readmem utility to tests

--- Makefile
+++ Makefile
@@ -3,8 +3,11 @@ CFLAGS  = -fPIC -pie -g -std=c99 -Werror -pedantic
 
 # list final targets
 PROG=gpio
+TESTS=readmem
 
 all: $(PROG)
+
+tests: $(TESTS)
 
 clean:
 	rm $(OBJS)
@@ -12,3 +15,6 @@ clean:
 
 gpio: src/inputs.c src/inputs.h
 	$(CC) $(CFLAGS) $< -o bin/$@
+
+readmem: test/readmem.c
+	$(CC) $(CFLAGS) $< -o ~/readmem
---
+++ test/readmem.c
@@ -0,0 +1,69 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+// Define macro values for the paging and block size
+#define PAGE_SIZE  (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+void printBinary(uint32_t* base)
+{
+	printf("|                                          |");
+	printf("   |                                          |");
+	printf("   |                                          |\n");
+	// printf("|           --- PINS 29-20 ---           |");
+	// printf("     |           --- PINS 19-10 ---           |");
+	// printf("     |            --- PINS 9-0 ---            |\n");
+	for (int j = 0; j < 3; j++)
+	{
+	  printf("|  ");
+	  for (int i = 9; i >= 0; i--)
+	  	printf(" %02d ", i + (2-j)*10);
+		printf("|   ");
+	}
+	printf("\n");
+	for (int j = 0; j < 3; j++)
+	{
+		uint32_t x = *(base + (3-j));
+	  for (int i = 0; i < 32; i++)
+	  {
+	  	if (!((i + 1) % 3)) printf(" ");
+	  	if (i > 1) {
+	  		printf("\x1b[3%d;1m", ((i-2)/3)%7 + 1);
+	  	}
+	  	printf("%d", 0x01 & (x >> (31 - i)));
+	  }
+		printf(" |   ");
+	}
+	printf("\x1b[0m\n");
+}
+
+#define BASE 0x20200000u
+
+int main(int argv, char** argc)
+{
+	if (argv != 2)
+  {
+  	fprintf(stderr, "Wrong number of arguments.");
+  	exit(EXIT_FAILURE);
+  }
+  //uint32_t addr = strtol(argc[1], NULL, 16);
+	//int addr = atoi(argc[1]);
+	int gpio_file = open("/dev/mem", O_RDWR|O_SYNC); 
+	void* gpioMap = mmap(
+    NULL,        // specific address not required
+    BLOCK_SIZE,  // the length of the mapping, to include
+                 // all the gpio reserved space
+    PROT_READ|PROT_WRITE,  // enable RW to memory
+    MAP_SHARED,  // share this location with other processes
+    gpio_file,      // specific file to open
+    BASE    // move to the GPIO reserved space by offset
+  );
+	close(gpio_file);
+	volatile unsigned *memword = (volatile unsigned *)gpioMap;
+	printBinary((uint32_t *)memword);
+	return 0;
+}
