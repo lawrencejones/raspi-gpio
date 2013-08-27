2b207924683d3d255f35280b5805390ce05d16ef
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-08-27 11:13:39 +0100

    Implements testing suite entry Bumped gitignore

--- i2c_devs/board.c
+++ i2c_devs/board.c
@@ -12,13 +12,12 @@
 #include "pca9548a.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-// TEST RUN
+// MUX TESTING
 ///////////////////////////////////////////////////////////////////////////////
 
 void mux_network_print(Mux *m);
-int main(int argc, char** argv)
+int mux_test(int argc, char** argv)
 {
-  printf("\n");
   // Initialise i2c protocol for bus 1
   // Assign i2c handle
   i2c_bus *i2c = i2c_init(1);
@@ -30,5 +29,34 @@ int main(int argc, char** argv)
   mux_network_print(m);
   // Dealloc the mux struct
   pca_dealloc(&m);
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// MAIN
+///////////////////////////////////////////////////////////////////////////////
+
+#define IS_FLAG(str) if (!strcmp(str, argv[1]))
+
+int main(int argc, char** argv)
+{
+  // Clear a line
+  printf("\n");
+  if (argc > 1)
+  {
+    // For the multiplexer tests
+    IS_FLAG("mux")
+    {
+      mux_test(argc, argv);
+    }
+    else
+    {
+      ERR("Test `%s` is not yet supported.\n\n", argv[1]);
+    }
+  }
+  else
+  {
+  ERR("Test flag is required.\n\n");
+  }
   return 0;
 }
